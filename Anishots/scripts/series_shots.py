c5b172e91931eef261d5bd888a8436ccaa993b31
jalexanu <nusaybah.alexander@jci.com>
2019-01-21 14:59:44 +0000

    outputs picture sequence to file, and increments file name

--- Anishots/scripts/series_shots.py
+++ Anishots/scripts/series_shots.py
@@ -1,31 +1,51 @@
 from mss import mss
 import mss.tools
 import os.path
-import pyautogui
+import win32api
+import time
+
 
 class ScreenShotMSS:
 
-    def screenshot(self, amount):
-        print('yo')
+    def screenshot(self, amount, i):
+
         with mss.mss() as sct:
             for _ in range(amount):
-                file = sct.shot(output ="stuff.png")
+                file = sct.shot(output ='run%s.png' %i)
                 print(file)
 
+class Optimiser:
+
+    def lossless(self):
+        pass
 
 class FileHandling:
     def on_exists(self, filename):
-        if os.path.isfile(filename):
+        if os.path.exists(filename):
             pass
 
+
+
 if __name__ == '__main__':
-    if pyautogui.click():
+    scrn = ScreenShotMSS()
+    i = 0
+    state_left = 1 #left button down
 
-        scrn = ScreenShotMSS()
-        scrn.screenshot(2)
+    time.sleep(1) #temp wait to ensure it doesn't run after the mouse event that clicks compile
+
+    while True:
+        a = win32api.GetKeyState(0x01)
+        if a != state_left:
+            pass
+        else:
+            i += 1
+            scrn.screenshot(1, i)
+
+        time.sleep(0.1)
 
 
-#TODO: ON first Click start taking acreenshots on second click stop
+
+#TODO: ON first Click(mouse event outside of program) start taking acreenshots on second click stop
 #TODO: output image to file
 #TODO: Image buffer
 #TODO: Optimise images in buffer
